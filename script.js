67711af22257d27c0d18102ff26277fb16cc1a95
Victoria Conlin <kinnevic@gmail.com>
2021-09-22 10:05:44 -0400

    Updated during review

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ num = "This is not a number"
 console.log(num);
 
 // 7
-let blank = undefined;
+let blank;
 // 8
 console.log(blank);
 // 9
 blank = null;
+console.log(blank);
 
 // Bonus 1
 const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
@@ -26,5 +27,7 @@ const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
 console.log(quarter, dime, nickel, penny);
 // Bonus 3
 let letters = "qwertyuiop";
-console.log(letters.length);
-console.log(letters[9]);
+// Bonus 4
+// console.log(letters.length);
+// console.log(letters[9]);
+console.log(letters[letters.length - 1]);
