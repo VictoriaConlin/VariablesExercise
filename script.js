e782c02504cbc68c2a7152d727769c03dbe00847
Victoria Conlin <kinnevic@gmail.com>
2021-09-22 08:57:37 -0400

    completed JavaScript Variables exercise, including bonus questions.

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1
+// const
+// 2 
+let num = 20;
+// 3
+num = 15;
+// 4
+// num = num + 7; 
+num +=7;
+// 5
+num--;
+// 6
+num = "This is not a number"
+console.log(num);
+
+// 7
+let blank = undefined;
+// 8
+console.log(blank);
+// 9
+blank = null;
+
+// Bonus 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// Bonus 2
+console.log(quarter, dime, nickel, penny);
+// Bonus 3
+let letters = "qwertyuiop";
+console.log(letters.length);
+console.log(letters[9]);
